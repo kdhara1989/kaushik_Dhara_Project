ab7ac2e2e798f490784db7229f12820dec5f498e
Kaushik Dhara <kaushikdhara@ymail.com>
2022-12-08 09:42:10 +0530

    refactor change

--- Project_Interview/src/main/java/com/cs/dao/LogDao.java
+++ Project_Interview/src/main/java/com/cs/dao/LogDao.java
@@ -2,17 +2,17 @@ package com.cs.dao;
 
 import java.io.FileNotFoundException;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import javax.sql.DataSource;
 import org.apache.log4j.Logger;
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.util.ResourceUtils;
 import com.cs.bean.LogBean;
 import static com.cs.constant.LogSQLConstant.SQL_CREATE;
@@ -25,62 +25,46 @@ import static com.cs.constant.LogSQLConstant.SQL_DELETE;
  *
  */
 public class LogDao {
-	
+
 	static Logger log = Logger.getLogger(LogDao.class.getName()); 
 	
+	private JdbcTemplate jdbcTemplate;
+
 	/**
 	 * Inserting log infos to DB
 	 * 
 	 * @param logs
 	 * @throws ClassNotFoundException 
 	 * @throws SQLException 
+	 * @throws FileNotFoundException 
 	 */
-	public void wirteInDB(List<LogBean> logs) throws ClassNotFoundException, SQLException {
-		Connection con = createConnection();
+	public void wirteInDB(List<LogBean> logs) throws ClassNotFoundException, SQLException, FileNotFoundException {
 		
-		if(createTable(con) == 0 ) deleteTable(con); 
-		
-		insertLogInfos(con, logs);
-		 
-		printTable(con);
-		closeConnection(con);
+		DataSource datasource = getDataSource();
+		setJdbcTemplate(new JdbcTemplate(datasource));
+
+		if(createTable(datasource.getConnection()) == 0 ) deleteTable(datasource.getConnection()); 
+
+		insertLogInfos(logs);
+
+		printTable();
 	}
 	
-	
 	/**
-	 * Creates connection objects for file based HSQLDB
+	 * Creates datasource objects for file based HSQLDB
 	 * 
 	 * @return
 	 */
-	private Connection createConnection() {
-		Connection con = null;
-		try {
-			Class.forName("org.hsqldb.jdbc.JDBCDriver");
-			con = DriverManager.getConnection("jdbc:hsqldb:" + ResourceUtils.getFile("src/main/resources/db/demodb").getAbsolutePath(), "SA", "");
-			if (con!= null){
-				log.info("Connection created successfully");
-			}else{
-				log.info("Problem with creating connection");
-			}
-		} catch (ClassNotFoundException | SQLException | FileNotFoundException e) {
-			log.error(e.getMessage(), e);
-		}
-		return con;
+	public static DriverManagerDataSource getDataSource() throws FileNotFoundException {
+
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.hsqldb.jdbc.JDBCDriver");
+		dataSource.setUrl("jdbc:hsqldb:" + ResourceUtils.getFile("src/main/resources/db/demodb").getAbsolutePath());
+		dataSource.setUsername("sa");
+		dataSource.setPassword("");
+		return dataSource;
 	}
-	
-	/**
-	 * Closing connection
-	 * 
-	 * @param con
-	 */
-	private void closeConnection(Connection con) {
-		try {
-			con.close();
-		} catch (SQLException e) {
-			log.error(e.getMessage(), e);
-		}
-	}
-	
+
 	/**
 	 * Creates table if not exist
 	 * 
@@ -99,8 +83,8 @@ public class LogDao {
 		}
 		return result;
 	}
-	
-	
+
+
 	/**
 	 * Cleans table to get it ready for next load
 	 * 
@@ -119,73 +103,81 @@ public class LogDao {
 		}
 		return result;
 	}
-	
-	
+
+
 	/**
 	 * Insert all log records in batch
 	 * 
 	 * @param con
 	 * @param logs
 	 */
-	private void insertLogInfos(Connection con, List<LogBean> logs) {
-		
+	private void insertLogInfos(List<LogBean> logs) {
+
 		final int batchSize = 500;
-		JdbcTemplate jdbcTemplate = new JdbcTemplate(new SingleConnectionDataSource(con, false));
-	    for (int j = 0; j < logs.size(); j += batchSize) {
+		for (int j = 0; j < logs.size(); j += batchSize) {
 
-	        final List<LogBean> batchList = logs.subList(j, j + batchSize > logs.size() ? logs.size() : j + batchSize);
-	        
-	        log.debug("Batch : Size - " + batchList.size() + " Start - " + j + " End - " + (j + batchList.size()));
+			final List<LogBean> batchList = logs.subList(j, j + batchSize > logs.size() ? logs.size() : j + batchSize);
 
-	        jdbcTemplate.batchUpdate(SQL_INSERT,
-	            new BatchPreparedStatementSetter() {
-	                @Override
-	                public void setValues(PreparedStatement ps, int i)
-	                        throws SQLException {
-	                	LogBean log = batchList.get(i);
-	                    ps.setString(1, log.getEventId());
-	                    ps.setString(2, log.getEventHost());
-	                    ps.setString(3, log.getEventType());
-	                    ps.setLong(4, log.getDuration());
-	                    ps.setBoolean(5, log.isAlert());
-	                }
+			log.debug("Batch : Size - " + batchList.size() + " Start - " + j + " End - " + (j + batchList.size()));
 
-	                @Override
-	                public int getBatchSize() {
-	                    return batchList.size();
-	                }
-	            });
-	    }
-	    log.info("Insertion successfull, tolat log proccessed - " + logs.size());
+			jdbcTemplate.batchUpdate(SQL_INSERT,
+					new BatchPreparedStatementSetter() {
+				@Override
+				public void setValues(PreparedStatement ps, int i)
+						throws SQLException {
+					LogBean log = batchList.get(i);
+					ps.setString(1, log.getEventId());
+					ps.setString(2, log.getEventHost());
+					ps.setString(3, log.getEventType());
+					ps.setLong(4, log.getDuration());
+					ps.setBoolean(5, log.isAlert());
+				}
+
+				@Override
+				public int getBatchSize() {
+					return batchList.size();
+				}
+			});
+		}
+		log.info("Insertion successfull, tolat log proccessed - " + logs.size());
 	}
-	
-	
+
+
 	/**
 	 * Optional - This method to print infos stored in DB table.
 	 * 
 	 * @param con
 	 */
-	private void printTable(Connection con) {
-		Statement stmt = null;
+	private void printTable() {
 		try {
-			stmt = con.createStatement();
-			ResultSet result = stmt.executeQuery(SQL_SELECT);
-
-			Optional.ofNullable(result).ifPresent( r -> {
-				log.info(" ===================================== Table ======================================= ");
-				log.info(" ID | HOST | TYPE | DURATION | ALERT ");
-				try {
-					while (r.next()) {
-						log.info(result.getString(1) + " | "+ result.getString(2) + " | " + result.getString(3) + " | " +  result.getLong(4) + " | " +  result.getBoolean(5));
-					}
-				} catch (SQLException e) {
-					log.error(e.getMessage(), e);
-				}
-				log.info(" =================================================================================== ");
+			List<LogBean> logs = getJdbcTemplate().query(SQL_SELECT, (rs, rowNum) -> {
+											LogBean log = new LogBean();
+											log.setEventId(rs.getString(1));
+											log.setEventHost(rs.getString(2));
+											log.setEventType(rs.getString(3));
+											log.setDuration(rs.getLong(4));
+											log.setAlert(rs.getBoolean(5));
+											return log;
+										});
+			log.info(" ===================================== Table ======================================= ");
+			log.info(" ID | HOST | TYPE | DURATION | ALERT ");
+			Optional.ofNullable(logs).orElse(Collections.emptyList()).forEach( l-> {
+				log.info(l.getEventId() + " | "+ l.getEventHost() + " | " + l.getEventType() + " | " + l.getDuration() + " | " + l.isAlert());
 			});
+			log.info(" =================================================================================== ");
 
 		}  catch (Exception e) {
 			log.error(e.getMessage(), e);
 		}
 	}
+
+	public JdbcTemplate getJdbcTemplate() {
+		return jdbcTemplate;
+	}
+
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+	
+	
 }
